4c35d2b41a08203a472899f726c0aab04ac0fe39
Admo9 <135870492+Admo9@users.noreply.github.com>
2023-08-25 21:09:56 -0500

    Update proyecto.ps1

--- proyecto.ps1
+++ proyecto.ps1
@@ -1,5 +1,6 @@
 ﻿#HAY QUE EJECUTAR COMO ADMINISTRADOR
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -force
+#powershell "start-process powershell -verb runas"
 #powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Admo9/Source/main/proyecto.ps1')"
 
 
