54547dbb7f33b108597cf68070390ac1e6ff0952
Admo9 <135870492+Admo9@users.noreply.github.com>
2024-06-02 18:24:43 -0500

    Update PruebaM.ps1

--- PruebaM.ps1
+++ PruebaM.ps1
@@ -1,1 +1,9 @@
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Install-PackageProvider -Name NuGet -force
+
+
 Install-Module -Name 7Zip4Powershell -force
