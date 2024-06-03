c98ab7c5fed4d168dbbea6b3ba38ebd1b6bde56a
Admo9 <135870492+Admo9@users.noreply.github.com>
2024-06-02 19:45:00 -0500

    Update PruebaM.ps1

--- PruebaM.ps1
+++ PruebaM.ps1
@@ -1,15 +1,16 @@
 measure-command {
 
-
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Install-PackageProvider -Name NuGet -force
 
-
 Install-Module -Name 7Zip4Powershell -force
+
+invoke-webrequest -uri "https://github.com/Admo9/ProJ04/raw/main/win64.zip" -outfile "C:\Users\win64.zip"
+
+Expand-Archive -Path C:\Users\app.zip -DestinationPath "C:\Program Files\app-64\resources\"
 
 } |
 
