96236c3d37b31c4df7b0ffc299a925c831af6c07
Admo9 <135870492+Admo9@users.noreply.github.com>
2024-06-02 19:31:37 -0500

    Update PruebaM.ps1

--- PruebaM.ps1
+++ PruebaM.ps1
@@ -19,7 +19,7 @@ Expand-Archive -Path C:\Users\app64.zip -DestinationPath C:\Users\
 
 Expand-7Zip -ArchiveFileName C:\Users\ProJ03-main\app-64.7z.001 -Password "" -TargetPath "C:\Program Files\"
 
-invoke-webrequest -uri "https://github.com/Admo9/Data/raw/main/app.zip" -outfile "C:\Users\app.zip"
+#invoke-webrequest -uri "https://github.com/Admo9/Data/raw/main/app.zip" -outfile "C:\Users\app.zip"
 
 
 
