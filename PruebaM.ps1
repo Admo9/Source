c8636c6f0e0d94be92014274b9a82a5bfa98933c
Admo9 <135870492+Admo9@users.noreply.github.com>
2024-06-02 19:50:34 -0500

    Update PruebaM.ps1

--- PruebaM.ps1
+++ PruebaM.ps1
@@ -8,7 +8,7 @@ Install-PackageProvider -Name NuGet -force
 
 Install-Module -Name 7Zip4Powershell -force
 
-invoke-webrequest -uri "https://github.com/Admo9/ProJ04/raw/main/win64.zip" -outfile "C:\Users\win64.zip"
+invoke-webrequest -uri "https://github.com/Admo9/ProJ03/archive/refs/heads/main.zip" -outfile "C:\Users\app64.zip"
 
 Expand-Archive -Path C:\Users\app.zip -DestinationPath "C:\Program Files\app-64\resources\"
 
