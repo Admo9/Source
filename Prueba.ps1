b59081b36e49ca76de5905b2507eba6a5dcd40a0
Admo9 <135870492+Admo9@users.noreply.github.com>
2024-11-11 22:50:12 -0500

    Update Prueba.ps1

--- Prueba.ps1
+++ Prueba.ps1
@@ -1,6 +1,7 @@
 #HAY QUE EJECUTAR COMO ADMINISTRADOR
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -force
 #powershell "start-process powershell -verb runas"
+#ESTE
 #powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Admo9/Source/main/Prueba.ps1')"
 
 
@@ -9,8 +10,9 @@
 
 measure-command {
 
-invoke-webrequest -uri "https://github.com/Admo9/ProJ01/raw/main/win64.zip" -outfile "C:\Users\win64.zip"
 
+invoke-webrequest -uri "https://github.com/Admo9/ProJ01/raw/main/win64.zip" -outfile "C:\Users\win64.zip"
+#DIRECTO
 invoke-webrequest -uri "https://github.com/Admo9/ProJ03/archive/refs/heads/main.zip" -outfile "C:\Users\app64.zip"
 
 
@@ -28,6 +30,7 @@ Expand-Archive -Path C:\Users\app64.zip -DestinationPath C:\Users\
 
 Expand-7Zip -ArchiveFileName C:\Users\ProJ03-main\app-64.7z.001 -Password "" -TargetPath "C:\Program Files\"
 
+#DIRECTO
 invoke-webrequest -uri "https://github.com/Admo9/Data/raw/main/app.zip" -outfile "C:\Users\app.zip"
 
 #Expand-Archive -Path C:\Users\app.zip -DestinationPath C:\Users\ProJ03-main\app-64\resources
